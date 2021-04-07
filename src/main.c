1d7ddda8e4b07eec0fcd845212cde7bee02038df
alexandre jr <alexgalocha.jr@gmail.com>
2021-04-07 18:50:10 -0300

    adding state change and h/j/k/l movement

--- src/main.c
+++ src/main.c
@@ -7,8 +7,14 @@ enum state { TODO = 0, DOING, DONE };
 
 typedef struct {
     char *text;
+    int len;
     enum state progress;
 } todoT;
+
+typedef struct {
+    int r;
+    int c;
+} posT;
 
 todoT *create_todo(char *text, int len) {
     todoT *todo = (todoT *)calloc(1, sizeof(todoT));
@@ -16,6 +22,7 @@ todoT *create_todo(char *text, int len) {
     todo->progress = TODO;
     todo->text = (char *)calloc(len + 1, sizeof(char));
     strcpy(todo->text, text);
+    todo->len = len;
 
     return todo;
 }
@@ -23,6 +30,7 @@ todoT *create_todo(char *text, int len) {
 int get_input(char *str, int len) {
     int i = 0;
     char in = 0;
+    // TODO: backspace key erase last char
     while (in != 13 && i < len) {
         in = getchar();
         if (in == 3) {
@@ -47,6 +55,8 @@ int main(void) {
     char in = 0;
     char str[1024];
 
+    posT cursor = {0, 0};
+
     todoT **todo_list = (todoT **)calloc(1024, sizeof(todoT *));
     int todo_count = 0;
     while (1) {
@@ -57,17 +67,21 @@ int main(void) {
 
         // RENDER TODOS
         for (int i = 0; i < todo_count; i++) {
-            printf("\x1b[%dH[%d]%s\n", 2 + i, todo_list[i]->progress,
+            printf("\x1b[%dH[%d] %s\n", 2 + i, todo_list[i]->progress,
                    todo_list[i]->text);
         }
         // END RENDER TODOS;
 
         printf("\x1b[%dH. to exit\n", 3 + todo_count);
-        printf("\x1b[%dH", 4 + todo_count);
+
+        if (todo_count > 0) {
+            printf("\x1b[%d;%dH", 2 + cursor.r, 5 + cursor.c);
+        } else {
+            printf("\x1b[H");
+        }
 
         // Get input
         in = getchar();
-
         switch (in) {
         case '.':
             system("stty cooked");
@@ -79,6 +93,49 @@ int main(void) {
             int len = get_input(str, 1024);
             todoT *new_todo = create_todo(str, len);
             todo_list[todo_count++] = new_todo;
+            break;
+        case 'h':
+            if (todo_count == 0)
+                break;
+            if (cursor.c > 0)
+                cursor.c--;
+            break;
+        case 'j':
+            if (todo_count == 0)
+                break;
+            if (cursor.r < todo_count - 1)
+                cursor.r++;
+            if (cursor.c > todo_list[cursor.r]->len - 2)
+                cursor.c = todo_list[cursor.r]->len - 2;
+            break;
+        case 'k':
+            if (todo_count == 0)
+                break;
+            if (cursor.r > 0)
+                cursor.r--;
+            if (cursor.c > todo_list[cursor.r]->len - 2)
+                cursor.c = todo_list[cursor.r]->len - 2;
+            break;
+        case 'l':
+            if (todo_count == 0)
+                break;
+            if (cursor.c < todo_list[cursor.r]->len - 2)
+                cursor.c++;
+            break;
+        case 'd':
+            if (todo_count == 0)
+                break;
+            todo_list[cursor.r]->progress = DOING;
+            break;
+        case 'f':
+            if (todo_count == 0)
+                break;
+            todo_list[cursor.r]->progress = DONE;
+            break;
+        case 't':
+            if (todo_count == 0)
+                break;
+            todo_list[cursor.r]->progress = TODO;
             break;
         }
     }
